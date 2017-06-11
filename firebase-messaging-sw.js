1f99710cbb250ae6db51f903579dddbe37d5560b
Roman Bolshakov <bolero2001@yandex.ru>
2017-06-11 12:12:32 +0200

    firebase: new version

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.1.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.1.2/firebase-messaging.js');
 
 firebase.initializeApp({
 	messagingSenderId: '754727428178'
