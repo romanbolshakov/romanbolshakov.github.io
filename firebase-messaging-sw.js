49e41df3b0703513a07e8b8a79716ef6fa601ccf
Roman Bolshakov <bolero2001@yandex.ru>
2017-06-11 12:31:17 +0200

    changes

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,13 @@
-importScripts('https://www.gstatic.com/firebasejs/4.1.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.1.2/firebase-messaging.js');
-
-firebase.initializeApp({
-	messagingSenderId: '754727428178'
+System.register("frontend/firebase-messaging-sw", [], function($__export) {
+  "use strict";
+  var firebaseMessaging;
+  return {
+    setters: [],
+    execute: function() {
+      importScripts('https://www.gstatic.com/firebasejs/4.1.2/firebase-app.js');
+      importScripts('https://www.gstatic.com/firebasejs/4.1.2/firebase-messaging.js');
+      firebase.initializeApp({messagingSenderId: '754727428178'});
+      firebaseMessaging = firebase.messaging();
+    }
+  };
 });
-
-const messaging = firebase.messaging();
