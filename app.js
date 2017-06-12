d9f53ea8315b8476822bddcfe523e8e664c6e7f6
Roman Bolshakov <bolero2001@yandex.ru>
2017-06-12 13:34:04 +0200

    changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 firebase.initializeApp({
-    messagingSenderId: '448358493027'
+    messagingSenderId: '754727428178'
 });
 
 
@@ -97,7 +97,7 @@ if (window.location.protocol === 'https:' &&
         ;
 
         // register fake ServiceWorker for show notification on mobile devices
-        navigator.serviceWorker.register('/serviceworker/messaging-sw.js');
+        navigator.serviceWorker.register('/messaging-sw.js');
         Notification.requestPermission(function(permission) {
             if (permission === 'granted') {
                 navigator.serviceWorker.ready.then(function(registration) {
--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,28 +2,28 @@ importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': '448358493027'
+    'messagingSenderId': '754727428178'
 });
 
 firebase.messaging();
 
-// self.addEventListener('notificationclick', function(event) {
-//     const target = event.notification.data.click_action || '/';
-//     event.notification.close();
+self.addEventListener('notificationclick', function(event) {
+    const target = event.notification.data.click_action || '/';
+    event.notification.close();
 
-//     // This looks to see if the current is already open and focuses if it is
-//     event.waitUntil(clients.matchAll({
-//         type: 'window',
-//         includeUncontrolled: true
-//     }).then(function(clientList) {
-//         // clientList always is empty?!
-//         for (var i = 0; i < clientList.length; i++) {
-//             var client = clientList[i];
-//             if (client.url == target && 'focus' in client) {
-//                 return client.focus();
-//             }
-//         }
+    // This looks to see if the current is already open and focuses if it is
+    event.waitUntil(clients.matchAll({
+        type: 'window',
+        includeUncontrolled: true
+    }).then(function(clientList) {
+        // clientList always is empty?!
+        for (var i = 0; i < clientList.length; i++) {
+            var client = clientList[i];
+            if (client.url == target && 'focus' in client) {
+                return client.focus();
+            }
+        }
     
-//         return clients.openWindow(target);
-//     }));
-// });
+        return clients.openWindow(target);
+    }));
+});
